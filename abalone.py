4f4b189026a0ad9691c37e624800aa48f1aef2f0
Cyriac <cyriac.thibaudeau@ens.uvsq.fr>
2024-04-04 11:44:36 +0200

    link menu + abalone + rect Circle

--- abalone.py
+++ abalone.py
@@ -1,9 +1,9 @@
-import pygame, sys
-
+import pygame
+import sys
 
 
 WINDOW_SIZE = (1280, 720)
-CELL_SIZE = 60
+CELL_SIZE = 70
 GRID_SIZE = 9
 
 GRID_WIDTH = GRID_SIZE * CELL_SIZE
@@ -14,70 +14,73 @@ RAYON = CELL_SIZE // 4
 
 WHITE = (255, 255, 255)
 VIDE = (101, 67, 32)
-BROWN = (139,69,19)
+BROWN = (139, 69, 19)
 BLACK = (0, 0, 0)
-BLUE = (0,0,255)
+BLUE = (0, 0, 255)
 BG = pygame.image.load('bg.png')
+
 
 class Plateau:
     """ 
     Classe Plateau qui permet de creer un plateau de jeu, c'est la class main de la gestion du jeu
     """
-    def __init__(self):
+
+    def __init__(self, SCREEN):
         """ 
         Constructeur de la classe Plateau
         """
         self.plateau = []
-        self.rang = [5,6,7,8,9,8,7,6,5]
-        self.init_plateau()
-    
-    def init_plateau(self):
+        self.rang = [5, 6, 7, 8, 9, 8, 7, 6, 5]
+        self.init_plateau(SCREEN)
+
+    def init_plateau(self, SCREEN):
         """
         Methode qui permet d'initialiser le plateau de jeu, elle permet de creer les billes et de les positionner sur le plateau.
         """
-        pygame.draw.circle(screen, BROWN, (WINDOW_SIZE[0]//2 ,WINDOW_SIZE[1]//2 ), MARGIN_X);"creer un cercle marron au centre de l'ecran"
-        schema = [[0,0,1,1,1,1,1,0,0],
-                [0,1,1,1,1,1,1,0,0],
-                [0,1,1,1,1,1,1,1,0],
-                [1,1,1,1,1,1,1,1,0],
-                [1,1,1,1,1,1,1,1,1],
-                [1,1,1,1,1,1,1,1,0],
-                [0,1,1,1,1,1,1,1,0],
-                [0,1,1,1,1,1,1,0,0],
-                [0,0,1,1,1,1,1,0,0]]; " matrice qui represente le plateau de jeu permet de savoir ou placer les billes sur le plateau"
+
+        schema = [[0, 0, 1, 1, 1, 1, 1, 0, 0],
+                  [0, 1, 1, 1, 1, 1, 1, 0, 0],
+                  [0, 1, 1, 1, 1, 1, 1, 1, 0],
+                  [1, 1, 1, 1, 1, 1, 1, 1, 0],
+                  [1, 1, 1, 1, 1, 1, 1, 1, 1],
+                  [1, 1, 1, 1, 1, 1, 1, 1, 0],
+                  [0, 1, 1, 1, 1, 1, 1, 1, 0],
+                  [0, 1, 1, 1, 1, 1, 1, 0, 0],
+                  [0, 0, 1, 1, 1, 1, 1, 0, 0]]
+        " matrice qui represente le plateau de jeu permet de savoir ou placer les billes sur le plateau"
         " position est la liste qui detient le departs des billes sur le plateau"
-        position =[-1, -1,-1, -1, -1, -1, -1, -1, -1, -1, -1, 0, 0, -1, -1, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        position = [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0, 0, -1, -1, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
         cpt = 0
         "c'est les main de la methode qui permet de creer les billes et de les positionner sur le plateau on ne cree une bille que si la valeur de la matrice est 1 et on choisi la couleur de la bille en fonction de la valeur de la liste position"
         for row in range(GRID_SIZE):
             for col in range(GRID_SIZE):
                 if schema[row][col] == 1:
                     if row % 2 == 1:
-                        x = MARGIN_X + col * CELL_SIZE + CELL_SIZE // 2 +30
+                        x = MARGIN_X + col * CELL_SIZE + CELL_SIZE // 2 + 30
                     else:
-                        x = MARGIN_X + col * CELL_SIZE + CELL_SIZE // 2 
+                        x = MARGIN_X + col * CELL_SIZE + CELL_SIZE // 2
                     y = MARGIN_Y + row * CELL_SIZE + CELL_SIZE // 2
                     if position[cpt] == -1:
-                        self.plateau.append(Bille(WHITE,x,y))
+                        self.plateau.append(Bille(SCREEN, WHITE, x, y))
                     elif position[cpt] == 0:
-                        self.plateau.append(Bille(VIDE,x,y))
+                        self.plateau.append(Bille(SCREEN, VIDE, x, y))
                     else:
-                        self.plateau.append(Bille(BLACK,x,y))
+                        self.plateau.append(Bille(SCREEN, BLACK, x, y))
                     cpt += 1
-
 
     def get_plateau(self):
         """
          retourne le plateau de jeu
         """
         return self.plateau
-    
+
     def get_rang(self):
         """
         retourne la liste des rangées
         """
         return self.rang
-    
+
     def __str__(self):
         """
         permet d'afficher le plateau de jeu
@@ -85,13 +88,13 @@ class Plateau:
         return "Plateau : " + str(self.plateau) + " Rang : " + str(self.rang)
 
 
-
-class Bille :
+class Bille:
     """
     Classe Bille qui permet de creer une bille
     """
-    def __init__(self,couleur,x,y):
-        self.id =  pygame.draw.circle(screen, couleur, (x, y), RAYON)
+
+    def __init__(self, SCREEN, couleur, x, y):
+        self.id = pygame.draw.circle(SCREEN, couleur, (x, y), RAYON)
         self.couleur = couleur
         self.x = x
         self.y = y
@@ -101,36 +104,6 @@ class Bille :
         retourne l'id de la bille
         """
         return self.id
-    
-    def get_couleur(self):
-        """
-        retourne la couleur de la bille
-        """
-        return self.couleur 
-    
-    def get_x(self):
-        """
-        retourne la position x de la bille
-        """
-        return self.x
-    
-    def get_y(self):
-        """
-        retourne la position y de la bille
-        """
-        return self.y
-    
-    def set_id(self,id):
-        """
-        permet de modifier l'id de la bille
-        """
-        self.id = id
-    
-    def set_couleur(self,couleur):
-        """
-        permet de modifier la couleur de la bille
-        """
-        self.couleur = couleur  
 
     def get_couleur(self):
         """
@@ -138,12 +111,36 @@ class Bille :
         """
         return self.couleur
 
+    def get_x(self):
+        """
+        retourne la position x de la bille
+        """
+        return self.x
+
+    def get_y(self):
+        """
+        retourne la position y de la bille
+        """
+        return self.y
+
+    def set_id(self, id):
+        """
+        permet de modifier l'id de la bille
+        """
+        self.id = id
+
+    def set_couleur(self, couleur):
+        """
+        permet de modifier la couleur de la bille
+        """
+        self.couleur = couleur
+
     def __str__(self):
         """
         permet d'afficher la bille
         """
         return "Bille id : " + str(self.id) + " Bille couleur : " + self.couleur
-    
+
     def __repr__(self):
         """
         permet d'afficher la bille
@@ -157,6 +154,7 @@ def deplacer_bille():
     """
     pass
 
+
 def distance(point1, point2):
     """
     Fonction qui permet de calculer la distance entre deux points, permet de reconnaitre si le curseur est dans la zoene d'une bille
@@ -164,8 +162,7 @@ def distance(point1, point2):
     return ((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2) ** 0.5
 
 
-
-def game():
+def game(SCREEN):
     """
     Fonction qui permet de lancer le jeu, elle permet de creer le plateau de jeu et de l'afficher 
     """
@@ -175,9 +172,14 @@ def game():
     running = True
     while running:
 
-        GAME_POS = pygame.mouse.get_pos()
+        SCREEN.fill(WHITE)
+        pygame.draw.circle(
+            SCREEN, BROWN, (WINDOW_SIZE[0]//2, WINDOW_SIZE[1]//2), WINDOW_SIZE[1]//2)
+        "creer un cercle marron au centre de l'ecran"
 
-        
+        positions = Plateau(SCREEN)
+
+        GAME_POS = pygame.mouse.get_pos()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -196,25 +198,17 @@ def game():
                         else:
                             print("Vous ne pouvez pas selectionner plus de 3 billes")
                             break
-            
-
-
-        screen.fill(WHITE)
-
-
-        positions = Plateau()
-
 
         pygame.display.flip()
 
     print(positions)
     pygame.quit()
     sys.exit()
-    
+
 
 if __name__ == "__main__":
     pygame.init()
-    screen = pygame.display.set_mode(WINDOW_SIZE)
-    screen.blit(BG, (0,0))
+    SCREEN = pygame.display.set_mode(WINDOW_SIZE)
+    SCREEN.blit(BG, (0, 0))
     pygame.display.set_caption("Matrice de Ronds")
-    game()
+    game(SCREEN)
--- menu.py
+++ menu.py
@@ -1,36 +1,40 @@
-import pygame, sys
+import pygame
+import sys
 import abalone
 
+
 class Button():
-	def __init__(self, image, pos, text_input, font, base_color, hovering_color):
-		self.image = image
-		self.x_pos = pos[0]
-		self.y_pos = pos[1]
-		self.font = font
-		self.base_color, self.hovering_color = base_color, hovering_color
-		self.text_input = text_input
-		self.text = self.font.render(self.text_input, True, self.base_color)
-		if self.image is None:
-			self.image = self.text
-		self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
-		self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
+    def __init__(self, image, pos, text_input, font, base_color, hovering_color):
+        self.image = image
+        self.x_pos = pos[0]
+        self.y_pos = pos[1]
+        self.font = font
+        self.base_color, self.hovering_color = base_color, hovering_color
+        self.text_input = text_input
+        self.text = self.font.render(self.text_input, True, self.base_color)
+        if self.image is None:
+            self.image = self.text
+        self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
+        self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
 
-	def update(self, screen):
-		if self.image is not None:
-			screen.blit(self.image, self.rect)
-		screen.blit(self.text, self.text_rect)
+    def update(self, screen):
+        if self.image is not None:
+            screen.blit(self.image, self.rect)
+        screen.blit(self.text, self.text_rect)
 
-	def checkForInput(self, position):
-		if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
-			return True
-		return False
+    def checkForInput(self, position):
+        if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
+            return True
+        return False
 
-	def changeColor(self, position):
-		if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
-			self.text = self.font.render(self.text_input, True, self.hovering_color)
-		else:
-			self.text = self.font.render(self.text_input, True, self.base_color)
-               
+    def changeColor(self, position):
+        if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
+            self.text = self.font.render(
+                self.text_input, True, self.hovering_color)
+        else:
+            self.text = self.font.render(
+                self.text_input, True, self.base_color)
+
 
 pygame.init()
 
@@ -44,27 +48,31 @@ BG = pygame.image.load('bg.png')
 def get_font(size):
     return pygame.font.Font("font.ttf", size)
 
+
 def main_menu():
     pygame.display.set_caption('Main Menu')
 
     while True:
-        SCREEN.blit(BG, (0,0))
+        SCREEN.blit(BG, (0, 0))
 
         MENU_MOUSE_POS = pygame.mouse.get_pos()
 
         MENU_TEXT = get_font(100).render('Main Menu', 1, (255, 255, 255))
         MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
 
-        PLAY_BUTTON = Button(image=None, pos=(640, 250), text_input="PLAY", font=get_font(75), base_color="#d7fcd4", hovering_color="Green")
-        OPTIONS_BUTTON = Button(image=None, pos=(640, 400), text_input="OPTIONS", font=get_font(75), base_color="#d7fcd4", hovering_color="Green")
-        QUIT_BUTTON = Button(image=None, pos=(640, 550), text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hovering_color="Green")
-        
+        PLAY_BUTTON = Button(image=None, pos=(640, 250), text_input="PLAY", font=get_font(
+            75), base_color="#d7fcd4", hovering_color="Green")
+        OPTIONS_BUTTON = Button(image=None, pos=(640, 400), text_input="OPTIONS", font=get_font(
+            75), base_color="#d7fcd4", hovering_color="Green")
+        QUIT_BUTTON = Button(image=None, pos=(640, 550), text_input="QUIT", font=get_font(
+            75), base_color="#d7fcd4", hovering_color="Green")
+
         SCREEN.blit(MENU_TEXT, MENU_RECT)
 
         for button in [PLAY_BUTTON, OPTIONS_BUTTON, QUIT_BUTTON]:
             button.changeColor(MENU_MOUSE_POS)
             button.update(SCREEN)
-        
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -80,17 +88,25 @@ def main_menu():
 
         pygame.display.update()
 
+
 def play():
     while True:
         PLAY_MOUSE_POS = pygame.mouse.get_pos()
 
         SCREEN.fill("black")
 
-        PLAY_TEXT = get_font(45).render("This is the PLAY screen.", True, "White")
+        PLAY_TEXT = get_font(45).render(
+            "This is the PLAY screen.", True, "White")
         PLAY_RECT = PLAY_TEXT.get_rect(center=(640, 260))
         SCREEN.blit(PLAY_TEXT, PLAY_RECT)
 
-        PLAY_BACK = Button(image=None, pos=(640, 460), text_input="BACK", font=get_font(75), base_color="White", hovering_color="Green")
+        GAME_BUTTON = Button(image=None, pos=(640, 360), text_input="PLAY GAME", font=get_font(
+            75), base_color="WHITE", hovering_color="Green")
+        PLAY_BACK = Button(image=None, pos=(640, 460), text_input="BACK", font=get_font(
+            75), base_color="White", hovering_color="Green")
+
+        GAME_BUTTON.changeColor(PLAY_MOUSE_POS)
+        GAME_BUTTON.update(SCREEN)
 
         PLAY_BACK.changeColor(PLAY_MOUSE_POS)
         PLAY_BACK.update(SCREEN)
@@ -100,6 +116,8 @@ def play():
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
+                if GAME_BUTTON.checkForInput(PLAY_MOUSE_POS):
+                    abalone.game(SCREEN)
                 if PLAY_BACK.checkForInput(PLAY_MOUSE_POS):
                     main_menu()
 
@@ -112,11 +130,13 @@ def options():
 
         SCREEN.fill("white")
 
-        OPTIONS_TEXT = get_font(45).render("This is the OPTIONS screen.", True, "Black")
+        OPTIONS_TEXT = get_font(45).render(
+            "This is the OPTIONS screen.", True, "Black")
         OPTIONS_RECT = OPTIONS_TEXT.get_rect(center=(640, 260))
         SCREEN.blit(OPTIONS_TEXT, OPTIONS_RECT)
 
-        OPTIONS_BACK = Button(image=None, pos=(640, 460), text_input="BACK", font=get_font(75), base_color="Black", hovering_color="Green")
+        OPTIONS_BACK = Button(image=None, pos=(640, 460), text_input="BACK", font=get_font(
+            75), base_color="Black", hovering_color="Green")
 
         OPTIONS_BACK.changeColor(OPTIONS_MOUSE_POS)
         OPTIONS_BACK.update(SCREEN)
@@ -130,5 +150,6 @@ def options():
                     main_menu()
 
         pygame.display.update()
+
 
 main_menu()
